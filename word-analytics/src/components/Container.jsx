e5a763425efc5d1a28dbe26ffa07648ceab20bae
Shin <vanvietkhtn@gmail.com>
2024-07-10 11:48:38 +0700

    floder structure

--- word-analytics/src/components/Container.jsx
+++ word-analytics/src/components/Container.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Textarea from './Textarea'
 import Stats from './Stats'
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from '../lib/constats'
 
 export default function Container() {
   const [text, setText] = useState('')
 
   const numberOfWords = text.split(/\s/).filter((word) => word !== '').length
   const numberOfCharacters = text.length
-  const instagramCharactersLeft = 280 - text.length
-  const facebookCharactersLeft = 2200 - text.length
+  const instagramCharactersLeft = INSTAGRAM_MAX_CHARACTERS - text.length
+  const facebookCharactersLeft = FACEBOOK_MAX_CHARACTERS - text.length
 
   const stats = {
     numberOfWords,
