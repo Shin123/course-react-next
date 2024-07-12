f9eebe6e26d94701718606d381990e4116124b1e
Shin <vanvietkhtn@gmail.com>
2024-07-12 16:33:58 +0700

    init project rmtDev

---
+++ rmtDev/src/components/JobListItem.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function JobListItem() {
+  return <div>JobListItem</div>
+}
---
+++ rmtDev/src/components/ResultsCount.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ResultsCount() {
+  return <div>ResultsCount</div>
+}
