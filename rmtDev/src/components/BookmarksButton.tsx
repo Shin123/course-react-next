a449fa5c7ce673a4566ec4b14fbdab1152dab265
Shin <vanvietkhtn@gmail.com>
2024-07-16 18:50:11 +0700

    Close Popover with claseName

--- rmtDev/src/components/BookmarksButton.tsx
+++ rmtDev/src/components/BookmarksButton.tsx
@@ -1,15 +1,39 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons'
 import BookmarksPopover from './BookmarksPopover'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false)
+
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const popoverRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        !e.target.closest('.bookmarks-btn') &&
+        !e.target.closest('.bookmarks-popover')
+      ) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  })
   return (
     <section>
-      <button onClick={() => setIsOpen(!isOpen)} className="bookmarks-btn">
+      <button
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+        className="bookmarks-btn"
+      >
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   )
 }
